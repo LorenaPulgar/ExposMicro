d9a09ccee0c95d5bac00da8e3b6c3e86b75025b9
Jose Quintero <cuellojose29@gmail.com>
2024-05-27 17:41:10 -0500

    discovery

---
+++ init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE bookingservice_db;
+CREATE DATABASE carservice_db;
+CREATE DATABASE customerservice_db;
+CREATE DATABASE paymentservice_db;
---
+++ src/main/java/com/expo/tallermicroexpo/TallerMicroExpoApplication.java
@@ -0,0 +1,13 @@
+package com.expo.tallermicroexpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerMicroExpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallerMicroExpoApplication.class, args);
+    }
+
+}
